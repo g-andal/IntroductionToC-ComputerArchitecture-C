dc49010a0d4fed26f50c629af3e718ff25df6a9f
gillian <gillianandal17@gmail.com>
2022-04-08 18:10:57 -0400

    works for 1 out of 2 test cases

--- sortArray.c
+++ sortArray.c
@@ -2,96 +2,148 @@
 #include <string.h>
 #include <stdlib.h>
 
+int* readfile(char*, int*, int*, int*);
+int* getNum(char [], int, int*, int*);
+int* splitNums(int*, int, int, int);
+void print(int*, int, int*, int);
 void sort(int [], int, int);
 
 int main(int argc, char *argv[] )  {
-    // open file
-    char *filename = argv[1];
-    FILE *fp = fopen(filename, "r");
+    int num_size=0, even_size=0, odd_size=0;
+    int* nums = readfile(argv[1], &num_size, &even_size, &odd_size);
 
-    // create input string array
-    char str[20];
+    int* even_nums = splitNums(nums, 0, num_size, even_size);
+    int* odd_nums = splitNums(nums, 1, num_size, odd_size);
 
-    // read first line and create number array
-    fgets(str, 20, fp);
+    sort(even_nums, 0, even_size - 1);
+    sort(odd_nums, 0, odd_size - 1);
 
-    int length = atoi(str);
-    int even[length];
-    int odd[length];
+    print(even_nums, even_size, odd_nums, odd_size);
 
-    // read second line (all of the elements)
-    fgets(str, 20, fp);
-    str[strcspn(str, "\n")] = 0;  // removes newline
-	
-    int init_size = strlen(str);
-	char delim[] = " ";
-
-	char *ptr = strtok(str, delim);
-    int new_num;
-    int even_i = 0; // keeps track of latest index in even array
-    int odd_i = 0;  // keeps track of latest index in odd array
-
-	while(ptr != NULL)      // while loop to break string apart at spaces
-	{
-        // changes num from string to int
-		new_num = atoi(ptr);
-
-        // places number into respective array (even or odd)
-        if(new_num % 2 == 0){
-            even[even_i] = new_num;
-            even_i++;
-        }
-        else{
-            odd[odd_i] = new_num;
-            odd_i++;
-        }
-        
-		ptr = strtok(NULL, delim);
-	}
-   
-   sort(even, 0, even_i - 1);
-   sort(odd, 0, odd_i - 1);
-
-    // print out even numbers
-	for (int i = 0; i < even_i; i++)
-	{
-		printf("%d ", even[i]);
-        
-	}
-
-    // print out odd numbers
-    for (int i = 0; i < odd_i; i++)
-	{
-		printf("%d ", odd[i]);
-	}
-	printf("\n");
-
-    // close the file
-    fclose(fp);
+    free(nums);
+    free(even_nums);
+    free(odd_nums);
 
     return 0;
 
 }
 
+// read file and return int array of all numbers
+int* readfile(char* file, int* arrsize, int* even, int* odd){
+    // open file
+    char *filename = file;
+    FILE *fp = fopen(filename, "r");
+
+    // create input string array
+    char str[50];
+
+    // read first line and create number array
+    fgets(str, 50, fp);
+
+    *arrsize = atoi(str);
+
+    // read second line (all of the elements)
+    fgets(str, 50, fp);
+    str[strcspn(str, "\n")] = 0;  // removes newline
+	
+    // close the file
+    fclose(fp);
+
+    return getNum(str, *arrsize, even, odd);
+}
+
+// creates int array from string
+int* getNum(char str[], int size, int* even, int* odd){
+    int* numlist = malloc(sizeof(int)*size);
+    int index = 0;
+    
+    char delim[] = "\t";
+	char *ptr = strtok(str, delim);
+
+    int new_num;
+    
+	while(ptr != NULL)      // while loop to break string apart at spaces
+	{
+        // changes num from string to int
+		new_num = atoi(ptr);
+        numlist[index] = new_num;
+        index++;
+
+        if(new_num % 2 == 0){
+            *even += 1;
+        }
+        else{
+            *odd += 1;
+        }
+
+		ptr = strtok(NULL, delim);
+	}
+
+    return numlist;
+}
+
+// returns array of only even or only odd numbers
+int* splitNums(int* arr, int rem, int oldsize, int newsize){
+    int* newArr = malloc(sizeof(int)*newsize);
+    int curIndex = 0;
+    
+    for(int i = 0; i < oldsize; i++){
+        if (rem == 0 && arr[i] % 2 == 0){
+            newArr[curIndex] = arr[i];
+            curIndex++;
+        }
+        else if(rem == 1 && arr[i] % 2 != 0){
+            newArr[curIndex] = arr[i];
+            curIndex++;
+        }
+    }
+    return newArr;    
+}
+
+// print out the arrays
+void print(int* even, int even_i, int* odd, int odd_i){
+    // print out even numbers
+	for (int i = 0; i < even_i; i++)
+	{
+		printf("%d\t", even[i]);
+	}
+
+    // print out odd numbers
+    for (int i = odd_i-1; i >= 0; i--)
+	{
+		printf("%d\t", odd[i]);
+	}
+	printf("\n");
+}
+
 // quick sort
-void sort(int arr[], int low, int high){
+void sort(int* arr, int low, int high){
     int pivot, low_i, high_i, temp;
+    
+    for (int i = 0; i < high+1; i++)
+	{
+		printf("%d\t", arr[i]);
+	}
+    printf("\n");
 
     // breaks recursion if low >= high
     if (low < high){
         pivot = low; // pivot will be first element
         low_i = low;
         high_i = high;
-
+        printf("pivot-%d\n", pivot);
+        printf("low-%d\n", low);
+        printf("high-%d\n", high);
         // keep going until we've seen all the elements in this partition
         while (low_i < high_i){
+            printf("lowi-%d \t highi-%d", low, high);
             // low index will keep incrementing as long as each element < pivot
-            while (arr[low_i] <= arr[pivot] && low_i <= high){
+            while (arr[low_i] <= arr[pivot] && low_i <= high_i){
                 low_i++;
             }
 
             // high index will keep decrementing as long as each element > pivot
-            while (arr[high_i] > arr[pivot] && high_i >= low){
+            while (arr[high_i] > arr[pivot] && high_i >= low_i){
                 high_i--;
             }
             
@@ -102,7 +154,7 @@ void sort(int arr[], int low, int high){
                 arr[high_i] = temp;
             }
         }
-    
+        printf("\n");
         // switch pivot and last element in the low section
         temp = arr[high_i];
         arr[high_i] = arr[pivot];
