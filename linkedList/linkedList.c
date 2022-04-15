0b60dd1a5052ffc9122d047409c07867ad465ed6
gillian <gillianandal17@gmail.com>
2022-04-15 17:37:52 -0400

    theoretically insert and delete work

--- linkedList/linkedList.c
+++ linkedList/linkedList.c
@@ -6,6 +6,9 @@
 struct node *head = NULL;
 struct node *current = NULL;
 
+void insert(int);
+void delete(int);
+
 int main(int argc, char *argv[] )  {
     FILE *fp = fopen(argv[1], "r");
     if (fp == NULL)
@@ -17,8 +20,18 @@ int main(int argc, char *argv[] )  {
     char instr; // i: insert; d: delete
     int num;    // value to add or delete
 
-    while(fscanf(fp, "%c\t%d", &instr, &num) != 0){
-        
+    while(!feof(fp)){
+        fscanf(fp, "%c\t%d\n", &instr, &num);
+        if (instr == 'i'){
+            printf("going into insert\n");
+            insert(num);
+            printf("succesful insert of %d\n", num);
+        }
+        else{
+            printf("going into delete\n");
+            delete(num);
+            printf("succesful delete of %d\n", num);
+        }
     }
 
     fclose(fp);
@@ -30,20 +43,50 @@ struct node {
    struct node *next;
 };
 
-//insert link at the first location
-void insertFirst(int data) {
-   //create a link
-   struct node *link = (struct node*) malloc(sizeof(struct node));
-	
-   link->data = data;
-	
-   //point it to old first node
-   link->next = head;
-	
-   //point first to new first node
-   head = link;
+void insert(int data) {
+    if(head == NULL || head->data >= data){
+        struct node *new = (struct node*) malloc(sizeof(struct node));
+	    new->data = data;
+        new->next = head;
+        head = new;
+        current = head;
+        return;
+    }
+
+    while(true){
+        if(current->next == NULL || current->next->data >= data){
+            struct node *new = (struct node*) malloc(sizeof(struct node));
+	        new->data = data;
+            new->next = current->next;
+            current->next = new;
+            break;
+        }
+        current = current->next;
+    }
+    current = head;
 }
 
+void delete(int data) {
+    if(head == NULL || head->data > data){
+        return;
+    }
+
+    if(head->data == data){
+        head = head->next;
+        current = head;
+        return;
+    }
+
+    while(current->next!= NULL){
+        if(current->next->data == data){
+            current->next = current->next->next;
+            break;
+        }
+        current = current->next;
+    }
+
+    current = head;
+}
 
 
 
